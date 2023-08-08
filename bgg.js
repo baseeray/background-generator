7d9254abbb5a3c0891d1a29b9af2bfecfcd50dc9
baseer <baseerr616@gmail.com>
2023-08-08 15:21:31 +0500

    Update bgg.js

--- bgg.js
+++ bgg.js
@@ -2,7 +2,7 @@ var css = document.querySelector("h3");
 var color1 =  document.getElementsByName("color1")[0];
 var color2 = document.getElementsByName("color2")[0];
 var body = document.getElementById("gradient");
-css.textContent = "linear-gradient(to right, rgb(0, 255, 34) , #00c3ff);";
+css.textContent = "linear-gradient(to right, rgb(0, 255, 34) , rgb(0, 195, 255));";
 
 function setGradient(){
     body.style.background = "linear-gradient(to right, "+color1.value+","+color2.value+")";
