751daa7970c47090bb79c98c59a97aeb4a671be7
baseer <baseerr616@gmail.com>
2023-08-26 14:01:09 +0500

    changing syntax to ES6

--- bgg.js
+++ bgg.js
@@ -1,10 +1,10 @@
-var css = document.querySelector("h3");
-var color1 =  document.getElementsByName("color1")[0];
-var color2 = document.getElementsByName("color2")[0];
-var body = document.getElementById("gradient");
+let css = document.querySelector("h3");
+let color1 =  document.getElementsByName("color1")[0];
+let color2 = document.getElementsByName("color2")[0];
+let body = document.getElementById("gradient");
 css.textContent = "linear-gradient(to right, rgb(0, 255, 34) , rgb(0, 195, 255));";
 
-function setGradient(){
+const setGradient = () => {
     body.style.background = "linear-gradient(to right, "+color1.value+","+color2.value+")";
     css.textContent = body.style.background+";";
 }
